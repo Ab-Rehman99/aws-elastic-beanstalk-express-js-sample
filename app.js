3d16d8c38301b8b21f2257f20b9b4597d20d2358
Abdul Rehman <i192190@lhr.nu.edu.pk>
2023-06-21 10:08:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,32 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Abdul Rehman. How are you?'));
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            text-align: center;
+          }
+          
+          h1 {
+            color: #333;
+            margin-top: 150px;
+            font-size: 48px;
+            text-shadow: 2px 2px 4px #000000;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Hello Everyone. How are you?</h1>
+      </body>
+    </html>
+  `);
+});
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
